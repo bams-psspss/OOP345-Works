91f80a653344dca616ad4ddaf91a37be7634469e
Pattarawan Saravaneeyawong <psaravaneeyawong@myseneca.ca>
2024-05-19 12:47:36 -0400

    Typo

--- Workshop01-P1/w1_p1.cpp
+++ Workshop01-P1/w1_p1.cpp
@@ -36,7 +36,7 @@ int main(int argc, char* argv[])
 	//   3: third argument
 	//   ...
 	for (auto i = 0; i < argc; i++) {
-		std::cout << i + 1 << ": " << argv[i] << std::endl;
+		std::cout << i + 1 << "  : " << argv[i] << std::endl;
 	}
 
 	std::cout << "--------------------------\n\n";
