39ed4514e2594a6524ffc62a86a3d784f1767c98
Pattarawan Saravaneeyawong <156383968+bams-psspss@users.noreply.github.com>
2024-05-26 09:46:01 -0400

    Update TimedTask.cpp

--- Workshop02/TimedTask.cpp
+++ Workshop02/TimedTask.cpp
@@ -1,3 +1,11 @@
+/***********************************************************************
+Name:		Pattarawan Saravaneeyawong
+Date:		26 May 2024
+StudentID:	130618234
+Email:		psaravaneeyawong@myseneca.ca
+
+I declar that this submission is the result of my own work and I only copied the code that my professor provided to complete my workshops and assignments. This submitted piece of work has not been shared with any other student or 3rd party content provider.
+***********************************************************************/
 #include <iostream>
 #include <iomanip>
 #include <cstring>
