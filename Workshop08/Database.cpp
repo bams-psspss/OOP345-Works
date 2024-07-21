56013195563efb7e0bd391db0e944fd8b67d5b54
Bam Pattarawan <psaravaneeyawong@myseneca.ca>
2024-07-21 19:25:41 -0400

    sdfsdfsdfd

--- Workshop08/Database.cpp
+++ Workshop08/Database.cpp
@@ -1,119 +1,12 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <algorithm>
-#include <memory>
-#include <iomanip>
-
 #include "Database.h"
 
-namespace seneca {
+// Explicit template instantiation for specific types
+template class seneca::Database<std::string>;
+template class seneca::Database<long long>;
 
-	std::shared_ptr<Database> Database::m_instance = nullptr;
-	std::string Database::m_keys[20];
-	std::string Database::m_values[20];
+// Explicit template specialization for encrypt/decrypt methods
+template <>
+void seneca::Database<std::string>::encryptDecrypt(std::string& value);
 
-	Database::Database(const std::string& filename) {
-		std::cout << "[" << this << "] "
-			<< "Database(const std::string&)\n";
-
-		//Open da FILE!
-		std::ifstream theFile(filename);
-
-		std::string tempKey, tempVal;
-		std::string theLine;
-
-		if (!theFile.is_open()) {
-			throw std::runtime_error("Bad FILE!!\n");
-		}
-		while (std::getline(theFile, theLine) && m_numEntries < 20) {
-			// Find the first space in the line to split key and value
-			size_t spacePos = theLine.find_first_of(' ');
-			if (spacePos != std::string::npos) {
-				std::string tempKey = theLine.substr(0, spacePos);
-				tempKey = trim(tempKey);
-
-				std::string tempVal = theLine.substr(spacePos + 1);
-				tempVal = trim(tempVal);
-
-				// Replace underscores in the key with spaces
-				std::replace(tempKey.begin(), tempKey.end(), '_', ' ');
-
-				// Store the key and value
-				m_keys[m_numEntries] = tempKey;
-				m_values[m_numEntries] = tempVal;
-
-				// Increment the number of entries
-				m_numEntries++;
-
-	
-			}
-		}
-	}
-
-	std::shared_ptr<Database> Database::getInstance(const std::string& filename)
-	{
-		if (m_instance == nullptr) {
-			m_instance = std::shared_ptr<Database> (new Database(filename));
-		}
-		return m_instance;
-	}
-
-	Err_Status Database::GetValue(const std::string& key, std::string& value)
-	{
-		for (size_t i = 0; i < m_numEntries; i++) {
-			if (key == m_keys[i]) {
-				value = m_values[i];
-				return Err_Status::Err_Success;
-			}
-		}
-		return Err_Status::Err_NotFound;
-	}
-
-	//Doubt
-	Err_Status Database::SetValue(const std::string& key, const std::string& value)
-	{
-		size_t theInd = 0;
-		if (m_numEntries < 20) {
-			for (size_t i = 0; i < m_numEntries && !(theInd); i++) {
-				if (m_keys[i].empty() && m_values[i].empty()) {
-					theInd = i;
-				}
-			}
-			m_keys[theInd] = key;
-			m_values[theInd] = value;
-			
-			m_numEntries++;
-			return Err_Status::Err_Success;
-		}
-
-		return Err_Status::Err_OutOfMemory;
-	}
-	std::string Database::trim(std::string theStr)
-	{
-		//Return npos if first position is NOT spacebar
-		size_t first = theStr.find_first_not_of(' ');
-
-		size_t last = theStr.find_last_not_of(' ');
-
-		if (first == std::string::npos) {
-			return theStr; //First char is not space return the aggrument
-		}
-
-		return theStr.substr(first, (last - first + 1));
-	}
-	Database::~Database()
-	{
-		std::cout << "[" << m_instance << "] ~Database()";
-
-
-		std::ofstream file(m_fileName + ".bkp.txt");
-		for (size_t i = 0; i < m_numEntries; i++)
-		{
-			file << std::setw(25) << std::left << m_keys[i] 
-				<< "-> " << m_values[i] << std::endl;
-		}
-
-	}
-}
-
+template <>
+void seneca::Database<long long>::encryptDecrypt(long long& value);
--- Workshop08/Database.h
+++ Workshop08/Database.h
@@ -3,59 +3,170 @@
 
 #include <iostream>
 #include <memory>
-#include <vector>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
+#include <sstream>
 
 namespace seneca {
 
-	enum class Err_Status
-	{
-		Err_Success,
-		Err_NotFound,
-		Err_OutOfMemory,
-	};
+    enum class Err_Status
+    {
+        Err_Success,
+        Err_NotFound,
+        Err_OutOfMemory,
+    };
 
-	class Database {
-		size_t m_numEntries{};
+    template <typename T>
+    class Database {
+        size_t m_numEntries{};
+        static std::shared_ptr<Database<T>> m_instance;
+        static std::string m_keys[20];
+        static T m_values[20];
+        std::string m_fileName{};
 
-		//Statically-allocated means the size cannot be change
-		//So no std::vector (vector can change size of array)
-		static std::string m_keys[20];
-		static std::string m_values[20];
-		std::string m_fileName{};
+        Database(const std::string& filename);
+        void encryptDecrypt(T& value);
 
-		//Shared pointer with the type of Databse
-		static std::shared_ptr<Database> m_instance;
+    public:
+        static std::shared_ptr<Database<T>> getInstance(const std::string& filename);
 
-		//Constructor //Not done yet
-		Database(const std::string& filename);
+        Err_Status GetValue(const std::string& key, T& value);
+        Err_Status SetValue(const std::string& key, const T& value);
 
-		template <typename T>
-		void encryptDecrypt(T& value);
+        std::string trim(std::string theString);
+        ~Database();
+    };
 
-		
-	public:
-		static std::shared_ptr<Database> getInstance(const std::string& filename);
+    // Template member definitions
 
-		//Find matching key with m_key and put set m_value to value
-		Err_Status GetValue(const std::string& key, std::string& value);
+    template <typename T>
+    std::shared_ptr<Database<T>> Database<T>::m_instance = nullptr;
 
-		//Insert value if the m_numEntries less than 20
-		//Doubt
-		Err_Status SetValue(const std::string& key, const std::string& value);
+    template <typename T>
+    std::string Database<T>::m_keys[20];
 
-		//Trim the whitespaces
-		std::string trim(std::string theString);
+    template <typename T>
+    T Database<T>::m_values[20];
 
-		//This too
-		~Database();
+    template <typename T>
+    Database<T>::Database(const std::string& filename) {
+        std::cout << "[" << this << "] Database(const std::string&)\n";
 
-	};
+        std::ifstream theFile(filename);
+        std::string tempKey, tempVal;
+        std::string theLine;
 
-	
+        if (!theFile.is_open()) {
+            throw std::runtime_error("Bad FILE!!\n");
+        }
+
+        while (std::getline(theFile, theLine) && m_numEntries < 20) {
+            size_t spacePos = theLine.find_first_of(' ');
+            if (spacePos != std::string::npos) {
+                tempKey = theLine.substr(0, spacePos);
+                tempKey = trim(tempKey);
+
+                tempVal = theLine.substr(spacePos + 1);
+                tempVal = trim(tempVal);
+
+                std::replace(tempKey.begin(), tempKey.end(), '_', ' ');
+
+                m_keys[m_numEntries] = tempKey;
+
+                std::istringstream ss(tempVal);
+                T tempValue;
+                ss >> tempValue;
+
+                encryptDecrypt(tempValue);
+                m_values[m_numEntries] = tempValue;
+
+                m_numEntries++;
+            }
+        }
+    }
+
+    template <typename T>
+    std::shared_ptr<Database<T>> Database<T>::getInstance(const std::string& filename) {
+        if (m_instance == nullptr) {
+            m_instance = std::shared_ptr<Database<T>>(new Database<T>(filename));
+        }
+        return m_instance;
+    }
+
+    template <typename T>
+    Err_Status Database<T>::GetValue(const std::string& key, T& value) {
+        for (size_t i = 0; i < m_numEntries; i++) {
+            if (key == m_keys[i]) {
+                value = m_values[i];
+                return Err_Status::Err_Success;
+            }
+        }
+        return Err_Status::Err_NotFound;
+    }
+
+    template <typename T>
+    Err_Status Database<T>::SetValue(const std::string& key, const T& value) {
+        if (m_numEntries < 20) {
+            m_keys[m_numEntries] = key;
+            m_values[m_numEntries] = value;
+            m_numEntries++;
+            return Err_Status::Err_Success;
+        }
+        return Err_Status::Err_OutOfMemory;
+    }
+
+    template <typename T>
+    std::string Database<T>::trim(std::string theStr) {
+        size_t first = theStr.find_first_not_of(' ');
+        size_t last = theStr.find_last_not_of(' ');
+
+        if (first == std::string::npos) {
+            return theStr;
+        }
+
+        return theStr.substr(first, (last - first + 1));
+    }
+
+    template <typename T>
+    void Database<T>::encryptDecrypt(T& value) {
+        // Default implementation does nothing
+    }
+
+    template <>
+    void Database<std::string>::encryptDecrypt(std::string& value) {
+        const char secret[]{ "secret encryption key" };
+
+        for (char& c : value) {
+            for (char k : secret) {
+                c = c ^ k;
+            }
+        }
+    }
+
+    template <>
+    void Database<long long>::encryptDecrypt(long long& value) {
+        const char secret[]{ "super secret encryption key" };
+
+        char* bytePtr = reinterpret_cast<char*>(&value);
+        for (std::size_t i = 0; i < sizeof(long long); ++i) {
+            for (char k : secret) {
+                bytePtr[i] = bytePtr[i] ^ k;
+            }
+        }
+    }
+
+    template <typename T>
+    Database<T>::~Database() {
+        std::cout << "[" << this << "] ~Database()\n";
+
+        std::ofstream file(m_fileName + ".bkp.txt");
+        for (size_t i = 0; i < m_numEntries; i++) {
+            file << std::setw(25) << std::left << m_keys[i]
+                << "-> " << m_values[i] << std::endl;
+        }
+    }
 }
 
-
-
-
-
-#endif // !SENECA_DATABASE_H
+#endif // SENECA_DATABASE_H
