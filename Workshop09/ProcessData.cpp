ed97aa34f72456e4c187fb435fb7f14d8f6f0057
Bam Pattarawan <psaravaneeyawong@myseneca.ca>
2024-07-27 19:44:04 -0400

    Workshop 09 P1 Testing

---
+++ Workshop09/ProcessData.cpp
@@ -0,0 +1,116 @@
+// Workshop 9 - Multi-Threading, Thread Class
+
+#include <iostream>
+#include <fstream>
+#include <functional>
+#include <string>
+#include "ProcessData.h"
+
+namespace seneca
+{
+	// The following function receives array (pointer) as the first argument, number of array 
+	//   elements (size) as second argument, divisor as the third argument, and avg as fourth argument. 
+	//   size and divisor are not necessarily same. When size and divisor hold same value, avg will 
+	//   hold average of the array elements. When they are different, avg will hold a value called 
+	// 	 as average-factor. For part 1, you will be using same value for size and double. Use of 
+	//   different values for size and divisor will be useful for multi-threaded implementation in part-2.
+	void computeAvgFactor(const int* arr, int size, int divisor, double& avg) {
+		avg = 0;
+		for (int i = 0; i < size; i++)
+		{
+			avg += arr[i];
+		}
+		avg /= divisor;
+	}
+	// The following function receives array (pointer) as the first argument, number of array elements  
+	//   (size) as second argument, divisor as the third argument, computed average value of the data items
+	//   as fourth argument, and var as fifth argument. Size and divisor are not necessarily same as in the 
+	//   case of computeAvgFactor. When size and divisor hold same value, var will get total variance of 
+	//   the array elements. When they are different, var will hold a value called as variance factor. 
+	//   For part 1, you will be using same value for size and double. Use of different values for size 
+	//   and divisor will be useful for multi-threaded implementation in part-2.
+	void computeVarFactor(const int* arr, int size, int divisor, double avg, double& var) {
+		var = 0;
+		for (int i = 0; i < size; i++)
+		{
+			var += (arr[i] - avg) * (arr[i] - avg);
+		}
+		var /= divisor;
+	}
+
+	ProcessData::operator bool() const {
+		return total_items > 0 && data != nullptr;
+	}
+
+	int ProcessData::operator()(const std::string& targetFile, double& avg, double& var) {
+		// Compute average value
+		computeAvgFactor(data, total_items, total_items, avg);
+
+		// Compute variance value
+		computeVarFactor(data, total_items, total_items, avg, var);
+
+		// Open the target file for writing in binary mode
+		std::ofstream outFile(targetFile, std::ios::binary);
+		if (outFile.is_open()) {
+			// Write the total number of items
+			outFile.write(reinterpret_cast<const char*>(&total_items), sizeof(int));
+
+			// Write the data items
+			outFile.write(reinterpret_cast<const char*>(data), total_items * sizeof(int));
+
+			// Close the file
+			outFile.close();
+			return 0;
+		}
+		else {
+			std::cerr << "Error opening file: " << targetFile << std::endl;
+			return -1;
+		}
+	}
+
+	// The following constructor of the functor receives name of the data file, opens it in
+	//   binary mode for reading, reads first int data as total_items, allocate memory space
+	//   to hold the data items, and reads the data items into the allocated memory space.
+	//   It prints first five data items and the last three data items as data samples.
+	ProcessData::ProcessData(const std::string& filename) {
+		// TODO: Open the file whose name was received as parameter and read the content
+		//         into variables "total_items" and "data". Don't forget to allocate
+		//         memory for "data".
+		//       The file is binary and has the format described in the specs.
+
+		std::ifstream theFile(filename, std::ios::binary);
+
+		if (theFile.is_open()) {
+
+			//Read and cast from binary to char
+			theFile.read(reinterpret_cast<char*>(&total_items), sizeof(int));
+
+			data = new int[total_items];
+
+			theFile.read(reinterpret_cast<char*>(data), total_items * sizeof(int));
+
+			theFile.close();
+		 
+		}
+		
+
+
+		std::cout << "Item's count in file '"<< filename << "': " << total_items << std::endl;
+		std::cout << "  [" << data[0] << ", " << data[1] << ", " << data[2] << ", ... , "
+		          << data[total_items - 3] << ", " << data[total_items - 2] << ", "
+		          << data[total_items - 1] << "]\n";
+	}
+
+	ProcessData::~ProcessData()
+	{
+		delete[] data;
+	}
+
+
+	// TODO Implement operator(). See workshop instructions for details.
+
+
+
+
+
+}
