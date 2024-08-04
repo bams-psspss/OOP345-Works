ea06e6d338c11f4f2491aed3c3bf1f3d0e332207
Pattarawan Saravaneeyawong <156383968+bams-psspss@users.noreply.github.com>
2024-08-03 20:02:28 -0400

    Add files via upload

---
+++ MS03/Utilities.h
@@ -0,0 +1,35 @@
+// Name: Pattarawan Saravaneeyawong
+// Seneca Student ID: 130618234
+// Seneca email: psaravaneeyawong@myseneca.ca
+// Date of completion: 20 Juy 2024
+//
+// I confirm that I am the only author of this file
+//   and the content was created entirely by me.
+#ifndef SENECA_UTILITIES_H
+#define SENECA_UTILITIES_H
+
+#include <string>
+
+namespace seneca {
+	class Utilities {
+		size_t m_widthField{1};
+		static char m_delimiter;
+
+	public:
+		void setFieldWidth(size_t newWidth);
+		size_t getFieldWidth() const;
+
+		std::string extractToken(const std::string& str, size_t& next_pos, bool& more);
+
+		static void setDelimiter(char newDelimiter);
+		static char getDelimiter();
+
+		std::string trim(std::string theStr);
+
+		
+	};
+}
+
+
+
+#endif // !SENECA_UTILITIES_H
